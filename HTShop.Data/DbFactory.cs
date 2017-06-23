3b4ed2982b1a9fbe554dc5123aa28cbe1812d59c
Nguyễn Lâm Anh Huy <huylnguyen@appvity.com>
2017-06-23 09:36:26 +0700

    update CodeFirst

---
+++ HTShop.Data/DbFactory.cs
@@ -0,0 +1,25 @@
+﻿using HTShop.Data.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTShop.Data
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private HTShopDbContext dbContext;
+
+        public HTShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new HTShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
